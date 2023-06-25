d278f9c5c73b28cc3089b3b97fbeca6bb5cdd867
Sandy Nascimento <sandymnascimento@outlook.com>
2023-06-25 20:21:25 -0300

    Initial commit

---
+++ Sistema Napster/cliente.py
@@ -0,0 +1,145 @@
+import threading
+import socket
+import json
+import os
+
+class cliente:
+    def __init__(self) -> None:
+        self.ip = input()
+        self.port = int(input())
+        self.data = {
+            'addrs_server' : (), # Endereço de conexão com o servidor.
+            'addrs_peer': (self.ip, self.port), #f"{self.ip}:{str(self.port)}", # Endereço disponível para outros clientes.
+            'folder_path' : input(), #Caminho para a pasta do cliente, onde os arquivos estão armazenados.
+            'files' : [] #Lista com o nome dos arquivos sob posse do cliente.
+        }
+        self.data['files'] = self.find_files()
+        self.search_peers = {}
+        self.start()
+
+
+    def start(self):
+        peer = socket.socket()
+        peer.connect(('127.0.0.1', 1099)) 
+
+        #Thread que irá permitir conexão entre clientes para download de arquivos.
+        threading.Thread(target=self.peer_server, args=()).start()
+        join = 0
+        while True:
+            try:
+                print(f"Digite a operacao que deseja realizar [JOIN | SEARCH | DOWNLOAD]:")
+                action = input()
+                #Envia a ação desejada ao servidor
+                peer.send(action.encode())
+                #Aciona o método responsável por executar a ação escolhida.
+                if (action.upper() == "JOIN" and join == 0):
+                    join += 1
+                    self.join_actions(peer)
+                elif(action.upper() == "SEARCH"):       
+                    self.search_actions(peer)
+                elif(action.upper() == 'DOWNLOAD'):
+                    self.download_actions(peer)
+                else:
+                    print("Operacao invalida, tente novamente")
+
+            except KeyboardInterrupt: 
+                peer.close()
+                break 
+
+    #Realiza a busca dos arquivos na pasta indicada        
+    def find_files(self):
+        folder = self.data['folder_path']
+        files = []
+        for name_file in os.listdir(folder):
+            # Verifica se é um arquivo
+            if os.path.isfile(os.path.join(folder, name_file)):
+                # Adiciona à lista de arquivos
+                files.append(name_file)
+        #Retorna uma lista com todos os arquivos.extensões exitentes na pasta do cliente.
+        return files   
+
+    #Método acionado para realizar as ações quando o cliente seleciona a opção "JOIN" no menu.         
+    def join_actions(self, peer):
+        #Envia ao servidor um dicionario com as informações do peer.
+        json_package = json.dumps(self.data)
+        peer.send(json_package.encode())
+        #Aguarda o retorno do "Join_OK"
+        response = peer.recv(1024).decode()
+        #Retorno solicitado e tratamento em caso de erro.
+        if(response.upper() == "JOIN_OK"):
+            print(f"Sou peer {self.data['addrs_peer'][0]}:{self.data['addrs_peer'][1]} com arquivos {' '. join(self.data['files'])}")
+        else:
+            print(f"Ocorreu um erro e o peer {self.data['addrs_peer'][0]}:{self.data['addrs_peer'][1]} nao estabeleceu conexao com o servidor")
+
+    def search_actions(self, peer):
+        file = input()
+        #Solicita o arquivo ao servidor
+        peer.send(file.encode())
+        #Espera a resposta com quais servidores possuem o arquivo
+        peers = json.loads(peer.recv(1024).decode())
+        print(f"peers com arquivo solicitado: {' '.join(peers)}")
+        #Armazena a informação obtida no search para realizar o download
+        if(len(peers) > 0): 
+            dicti = {
+                'name'  : file,
+                'peers' : peers
+            }
+            self.search_peers = dicti
+        return
+
+    def download_actions(self, peer):
+        ip = input()
+        port = input()
+        server = f"{ip}:{port}"
+        
+        try:
+            #Verifica se o endereço informado é válido como fonte do arquivo desejado
+            if(server in self.search_peers['peers']):
+                s = socket.socket()
+                s.connect((ip,int(port)))
+                
+                if(s.recv(1024).decode() == "Connect_OK"):
+                    s.send(self.search_peers['name'].encode())
+                    # Abra o arquivo em modo de escrita binária
+                    with open(self.data['folder_path'] + "\\" + self.search_peers['name'], 'wb') as arquivo:
+                        while True:
+                            # Recebe uma parte do arquivo
+                            dados = s.recv(4096)
+                            if not dados:
+                                # Todos os dados foram lidos
+                                break
+                            #Escreve o dado no arquivo
+                            arquivo.write(dados)
+                    print(f"Arquivo {self.search_peers['name']} baixado com sucesso na pasta {os.path.basename(self.data['folder_path'])}")
+                    peer.send("UPDATE".encode())
+                    peer.send(self.search_peers['name'].encode())
+                    self.data['files'] = self.find_files()
+        except KeyError:
+            return
+
+
+    def peer_server(self):
+        server = socket.socket()
+        server.bind((self.data['addrs_peer'][0], self.data['addrs_peer'][1]))
+        server.listen()
+
+        while True:
+            peer, addr = server.accept()
+            peer.send("Connect_OK".encode())
+            file = peer.recv(1024).decode()
+            if(file in self.data['files']):
+                # Abra o arquivo em modo de leitura binária
+                with open(self.data['folder_path'] + "\\" + file, 'rb') as arquivo:
+                    while True:
+                        # Leia uma parte do arquivo
+                        dados = arquivo.read(4096)
+                        if not dados:
+                            # Todos os dados foram lidos
+                            break
+                        # Envie os dados para o servidor
+                        peer.send(dados)
+
+            peer.close()
+        server.close()
+
+cliente()            
---
+++ Sistema Napster/servidor.py
@@ -0,0 +1,84 @@
+import threading
+import socket
+import json
+
+class servidor:
+    def __init__(self):
+        self.ip_server = input() 
+        self.port_server = int(input()) 
+        self.infos_peers = []
+        self.start()
+
+
+    def start(self):
+        #Criação do socket, bind do endereço e abertura do server aguardando conexões.
+        server = socket.socket()
+        server.bind(('', self.port_server))
+        server.listen()
+
+        while True:
+            try:
+                #Aceita conexão e inicia uma thread para conseguir atender múltiplos clientes.
+                peer, addr = server.accept()
+                threading.Thread(target=self.start_connection, args=(peer, addr,)).start()
+                
+            except KeyboardInterrupt:
+                server.close()
+                break
+        
+        
+    def start_connection(self, peer, addr):    
+        while True:   
+            try:
+                #Aguarda mensagem do cliente informando qual operação deseja realizar
+                action = peer.recv(1024).decode()
+                    
+                if(action.upper() == "JOIN"):
+                    self.join_actions(peer, addr)
+
+                elif(action.upper() == "SEARCH"):
+                    self.search_actions(peer, addr)
+
+                elif(action.upper() == "UPDATE"):
+                    self.update_actions(peer, addr)
+
+            except KeyboardInterrupt:
+                raise
+
+    def join_actions(self, peer, addr):
+        #Aguarda o recebimento das informações do cliente.
+        response = json.loads(peer.recv(1024).decode())
+        #Armazena o endereço gerado pela conexão cliente-servidor junto das informações do cliente.
+        response['addrs_server'] = addr
+        response['addrs_peer'] = f"{response['addrs_peer'][0]}:{response['addrs_peer'][1]}"
+        #Armazena as informações do cliente.
+        self.infos_peers.append(response)
+        print(f"Peer {response['addrs_peer']} adicionado com arquivos {' '.join(response['files'])}")
+        #Confirma que o Join foi bem sucedido.
+        peer.send("JOIN_OK".encode())
+    
+    def search_actions(self, peer, addr):
+        #Aguarda o cliente dizer qual arquivo está buscando
+        response = peer.recv(1024).decode()
+        lista = []
+        # Percorrendo a lista de dicionários
+        for client in self.infos_peers:
+            #IF utilizado para exibir a mensagem que informa o peer e o arquivo solicitado
+            if(client['addrs_server'] == addr):
+                print(f"Peer {client['addrs_peer']} solicitou arquivo {response}")
+            #IF que verifica quais peers possuem o arquivo solicitado e adiciona a uma lista
+            if(response in client['files']):
+                lista.append(client['addrs_peer'])
+        #Envia lista com todos os peers que possuem o arquivo para o cliente que realizou a solicitação
+        peer.send(json.dumps(lista).encode())
+
+    def update_actions(self, peer, addr):
+        #Aguarda mensagem informando qual o nome do arquivo que foi adicionado a pasta do peer.
+        response = peer.recv(1024).decode()
+        for client in self.infos_peers:
+            #Realiza a busca do peer dentre os disponíveis e adiciona na sua lista de arquivos o novo conteúdo.
+            if(client['addrs_server'] == addr):
+                client['files'].append(response)
+        
+
+servidor()
